74cbaca211837fc346b141cb8b6da37cbe41b48f
ismailaksoyes <ismailaksoyes@gmail.com>
2021-02-25 23:37:50 +0300

    Update Fragmnet

--- app/src/main/java/com/avalon/calizer/ui/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/avalon/calizer/ui/tutorial/TutorialFragment.kt
@@ -7,15 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
 import android.view.animation.TranslateAnimation
 import android.widget.TextView
+
 import androidx.core.view.isVisible
-import androidx.viewpager.widget.ViewPager
+
 import androidx.viewpager2.widget.ViewPager2
 import com.avalon.calizer.R
 import com.avalon.calizer.databinding.FragmentTutorialBinding
-import com.avalon.calizer.databinding.ProfileFragmentBinding
-import com.avalon.calizer.ui.accounts.adapters.AccountsAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -45,6 +46,7 @@ class TutorialFragment : Fragment() {
         viewPager.adapter = tutorialAdapter
         viewPagerControl()
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -52,29 +54,35 @@ class TutorialFragment : Fragment() {
         binding = FragmentTutorialBinding.inflate(inflater, container, false)
         return binding.root
     }
-    fun viewPagerControl(){
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+    fun viewPagerControl() {
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
                 positionOffsetPixels: Int
             ) {
-                Log.d("position","$position offset $positionOffset")
-                when(position){
-                    0->{
-                        if(!binding.tvNextIntro.isVisible){
+                Log.d("position", "$position offset $positionOffset")
+                when (position) {
+                    0 -> {
+                        if (!binding.tvNextIntro.isVisible) {
 
-                           // binding.tvNextIntro.visibility = View.VISIBLE
+                            // binding.tvNextIntro.visibility = View.VISIBLE
                             binding.tvNextIntro.visible()
 
                         }
-                        if(!binding.tvIntroDesc.isVisible){
+                        if (!binding.tvIntroDesc.isVisible) {
                             binding.tvIntroDesc.visibility = View.VISIBLE
                         }
+                        if(!binding.clStartNowNext.isVisible){
+                            binding.clStartNowNext.visibility = View.VISIBLE
+                            binding.ivStartNowImage.visibility = View.VISIBLE
+                            binding.tvStartNow.visibility = View.VISIBLE
+                        }
                     }
-                    1-> {
-                        if(binding.tvNextIntro.isVisible){
-                          //  binding.tvNextIntro.visibility = View.GONE
+                    1 -> {
+                        if (binding.tvNextIntro.isVisible) {
+                            //  binding.tvNextIntro.visibility = View.GONE
                             binding.tvNextIntro.gone()
                         }
                     }
@@ -83,50 +91,21 @@ class TutorialFragment : Fragment() {
             }
         })
     }
-    fun TextView.visible(){
+
+
+    fun TextView.visible() {
         visibility = View.VISIBLE
-        val animate = TranslateAnimation(0f, 0f, 20f, 0f)
-        animate.duration = 700
-       // animate.fillAfter = true
-        this.startAnimation(animate)
-        animate.setAnimationListener(object : Animation.AnimationListener{
-            override fun onAnimationStart(animation: Animation?) {
-                alpha = 0f
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                alpha = 1f
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-
-        })
+        val animation = AnimationUtils.loadAnimation(this.context, R.anim.fade_in)
+        this.startAnimation(animation)
 
     }
-    fun TextView.gone(){
-        //visibility = View.GONE
-        val animate = TranslateAnimation(0f, 0f, 0f,20f)
-        animate.duration = 500
-       // animate.fillAfter = true
-        this.startAnimation(animate)
-        animate.setAnimationListener(object :Animation.AnimationListener{
-            override fun onAnimationStart(animation: Animation?) {
-                alpha = 1f
-            }
 
-            override fun onAnimationEnd(animation: Animation?) {
-                  post(Runnable { visibility = View.GONE })
-
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-        })
-
-
+    fun TextView.gone() {
+        val animation = AnimationUtils.loadAnimation(this.context, R.anim.fade_out)
+        this.startAnimation(animation)
+        postDelayed({
+            visibility = View.GONE
+        }, 500)
     }
 
     companion object {
