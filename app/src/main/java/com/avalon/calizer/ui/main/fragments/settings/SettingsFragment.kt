58b5d8eeee6c7d4dd6d2afe715794e5aa638023c
ismailaksoyes <ismailaksoyes@gmail.com>
2021-03-11 21:32:37 +0300

    Add Settings Prefs v2

--- app/src/main/java/com/avalon/calizer/ui/main/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/avalon/calizer/ui/main/fragments/settings/SettingsFragment.kt
@@ -2,40 +2,30 @@ package com.avalon.calizer.ui.main.fragments.settings
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+
+import androidx.preference.PreferenceFragmentCompat
 import com.avalon.calizer.R
-import kotlinx.android.synthetic.main.settings_fragment.*
 
-class SettingsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
+class SettingsFragment : PreferenceFragmentCompat() {
+
+
 
     private lateinit var viewModel: SettingsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
-    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        tv_settings.setOnClickListener {
-           // it.findNavController().navigate(R.id.action_destination_settings_to_viewPagerFragment)
-
-        }
 
 
+
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_settings,rootKey)
     }
 
 }
