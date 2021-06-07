b6b6a4e6b920da2384c2309097b49027a5a7aa63
ismailaksoyes <ismailaksoyes@gmail.com>
2021-06-07 12:35:27 +0300

    Update Paging Source

--- app/src/main/java/com/avalon/calizer/data/datasource/FollowDataPagingSource.kt
+++ app/src/main/java/com/avalon/calizer/data/datasource/FollowDataPagingSource.kt
@@ -7,11 +7,19 @@ import com.avalon.calizer.data.repository.RoomRepository
 import javax.inject.Inject
 
 class FollowDataPagingSource @Inject constructor(private val repository: RoomRepository):PagingSource<Int, FollowData>() {
-    override fun getRefreshKey(state: PagingState<Int, FollowData>): Int? {
-        TODO("Not yet implemented")
-    }
+
+
+    override fun getRefreshKey(state: PagingState<Int, FollowData>): Int? = null
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, FollowData> {
-        TODO("Not yet implemented")
+       val position = params.key ?: STARTING_INDEX
+        val follow = repository.getFollowersData(0,params.loadSize)
+       return LoadResult.Page(
+            data = follow,
+           prevKey = if (position == STARTING_INDEX) null else position -1,
+           nextKey = if (follow.isNullOrEmpty()) null else position +1
+
+        )
     }
+companion object{const val STARTING_INDEX = 0}
 }
