ab4e9d9d1a72f40375e4c387b03c7b5a9afa1dbd
ismailaksoyes <ismailaksoyes@gmail.com>
2021-07-15 22:53:24 +0300

    #9 Add activity for result

--- app/src/main/java/com/avalon/calizer/ui/main/fragments/profile/photocmp/PhotoUploadFragment.kt
+++ app/src/main/java/com/avalon/calizer/ui/main/fragments/profile/photocmp/PhotoUploadFragment.kt
@@ -1,5 +1,7 @@
 package com.avalon.calizer.ui.main.fragments.profile.photocmp
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
@@ -8,6 +10,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -28,12 +32,18 @@ class PhotoUploadFragment : Fragment() {
     @Inject
     lateinit var viewModel: PhotoAnalyzeViewModel
 
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentPhotoUploadBinding.inflate(inflater,container,false)
         return binding.root
     }
@@ -44,6 +54,16 @@ class PhotoUploadFragment : Fragment() {
         urlList.add("https://witandfolly.co/wp-content/uploads/2020/01/Hoka-14-1-scaled.jpg")
         setGlideImageUrl(urlList)
         RealTimeData()
+
+    }
+
+    fun getGalleryImage(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
+        intent.action = Intent.ACTION_GET_CONTENT
+       startForResult.launch(intent)
+
     }
 
     fun getBitmap(bitmap: Bitmap){
@@ -54,8 +74,9 @@ class PhotoUploadFragment : Fragment() {
        val newList =  list.toTypedArray()
         viewModel.setPhotoData(list)
         binding.btnUploadImage.setOnClickListener {
-           val action = PhotoUploadFragmentDirections.actionPhotoUploadFragmentToPhotoAnalyzeLoadingFragment(newList)
-          findNavController().navigate(action)
+          // val action = PhotoUploadFragmentDirections.actionPhotoUploadFragmentToPhotoAnalyzeLoadingFragment(newList)
+          //findNavController().navigate(action)
+            getGalleryImage()
         }
 
     }
---
+++ app/src/main/java/com/avalon/calizer/ui/main/fragments/profile/photocmp/PhotoUploadObserver.kt
@@ -0,0 +1,9 @@
+package com.avalon.calizer.ui.main.fragments.profile.photocmp
+
+import androidx.activity.result.ActivityResultRegistry
+import androidx.lifecycle.LifecycleObserver
+import javax.inject.Inject
+
+class PhotoUploadObserver @Inject constructor(private val registry: ActivityResultRegistry):LifecycleObserver {
+
+}
